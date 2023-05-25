9718431b66023cf9132eee84c87786b0e617c14b
sumito0 <chensaquilsuany@cticoban.edu.gt>
2023-05-25 09:25:34 -0600

    nimodo

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.news-cards1', {delay: 500});
+ScrollReveal().reveal('.imagen1', {delay: 500});
+ScrollReveal().reveal('.imagen2', {delay: 500});
+ScrollReveal().reveal('.imagen3', {delay: 500});
+ScrollReveal().reveal('.imagen4', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 500});
